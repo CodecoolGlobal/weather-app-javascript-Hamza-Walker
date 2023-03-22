970ab92940a30c9d6194e7e30f059a9ee736b690
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-03-22 05:56:39 +0100

    added some css and refractored the code to be more readable

--- frontend/script.js
+++ frontend/script.js
@@ -1,42 +1,54 @@
 const apiKey = "e64620c4c83e4a1d90095232232103";
 let selectedCity = "";
 let apiUrl = `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${selectedCity}`;
+
 const cityNames = cities.map(city => city.name);
 const uniqueCityNames = [...new Set(cityNames)];
 
+const MAX_SUGGESTIONS = 8;
+const input = document.getElementById("city");
+const suggestions = document.getElementById("suggestions");
+
 const autocompleteInputField = () => {
-	
-	const input = document.getElementById("city");
-	const suggestions = document.getElementById("suggestions");
-	const MAX_SUGGESTIONS = 8;
-	
-	input.addEventListener("input", () => {
-	  const userInput = input.value;
-	  const matches = uniqueCityNames.filter(city => city.toLowerCase().startsWith(userInput.toLowerCase())).slice(0, MAX_SUGGESTIONS);
-	
-	  if (matches.length > 0) {
-	    suggestions.innerHTML = "";
-	    
-	    matches.forEach(match => {
-	      const suggestion = document.createElement("div");
-	      suggestion.classList.add("suggestion");
-	      suggestion.textContent = match;
-	      suggestion.addEventListener("click", () => {
-	        input.value = match;
-	        selectedCity = match;
-	        apiUrl = `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${selectedCity}`; // update apiUrl
-	        suggestions.innerHTML = "";
-	        console.log(selectedCity)
-            callApiWithCityMatch(apiUrl) 
-	      });
-	      suggestions.appendChild(suggestion);
-	    });
-	  } else {
-	    suggestions.innerHTML = "";
-	  }
-	});
+    input.addEventListener("input", handleInput);
 }
-autocompleteInputField()
+
+const handleInput = () => {
+    const userInput = input.value;
+    const matches = uniqueCityNames.filter(city => city.toLowerCase().startsWith(userInput.toLowerCase())).slice(0, MAX_SUGGESTIONS);
+
+    if (matches.length > 0) {
+        clearSuggestions();
+        matches.forEach(match => {
+            const suggestion = createSuggestion(match);
+            suggestion.addEventListener("click", handleSuggestionClick);
+            suggestions.appendChild(suggestion);
+        });
+    } else {
+        clearSuggestions();
+    }
+}
+
+const clearSuggestions = () => {
+    suggestions.innerHTML = "";
+}
+
+const createSuggestion = (match) => {
+    const suggestion = document.createElement("div");
+    suggestion.classList.add("suggestion");
+    suggestion.textContent = match;
+    return suggestion;
+}
+
+const handleSuggestionClick = (event) => {
+    const suggestion = event.target;
+    input.value = suggestion.textContent;
+    selectedCity = suggestion.textContent;
+    apiUrl = `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${selectedCity}`;
+    clearSuggestions();
+    console.log(selectedCity);
+    callApiWithCityMatch(apiUrl);
+}
 
 const callApiWithCityMatch = (apiUrl) => {
     fetch(apiUrl)
@@ -46,3 +58,5 @@ const callApiWithCityMatch = (apiUrl) => {
         })
         .catch(error => console.error(error));
 }
+
+autocompleteInputField();
